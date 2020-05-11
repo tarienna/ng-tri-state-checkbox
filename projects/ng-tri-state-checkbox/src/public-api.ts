1912cf137b265a9c08975f2ba5d4b05c8a3c6eff
Philipp Donabauer <philipp.donabauer@tarienna.eu>
2020-05-11 16:09:09 +0200

    Packakge.json was changed to support angular from version 9.0.0

--- projects/ng-tri-state-checkbox/src/public-api.ts
+++ projects/ng-tri-state-checkbox/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of ng-tri-state-checkbox
  */
 export * from './lib/ng-tri-state-checkbox.component';
+export * from './lib/ng-tri-state-control.directive';
 export * from './lib/ng-tri-state-checkbox.module';
